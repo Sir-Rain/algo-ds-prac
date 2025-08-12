6cc6c667c3c057aaa23e5b30eaf357c05e3b006e
Sir-Rain <liciaan@kakao.com>
2025-08-12 13:36:51 +0000

    DS array based stack with procedural rust simple test

--- Data-Structure/Stack/Array-Based/rust-procedural/main.rs
+++ Data-Structure/Stack/Array-Based/rust-procedural/main.rs
@@ -1,3 +1,27 @@
+mod array_stack;
+
+use array_stack::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut array_stack = create_stack(10);
+
+    push(&mut array_stack, 5);
+    push(&mut array_stack, 3);
+    push(&mut array_stack, 4);
+    push(&mut array_stack, 8);
+    push(&mut array_stack, 10);
+
+    let data1 = top(&array_stack).unwrap();
+
+    println!("{data1}");
+
+    pop(&mut array_stack);
+
+    let data2 = pop(&mut array_stack).unwrap();
+
+    println!("{data2}");
+
+    println!("{}", get_stack_size(&array_stack));
+
+    println!("{}", is_empty(&array_stack));
 }
