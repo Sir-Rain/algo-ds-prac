923f6374ecfb9a3fad53baf94112f9e558413376
Sir-Rain <liciaan@kakao.com>
2025-08-15 13:30:48 +0000

    DS heap with rs implementation

--- Data-Structure/Tree/Heap/rust-procedural/main.rs
+++ Data-Structure/Tree/Heap/rust-procedural/main.rs
@@ -1,3 +1,23 @@
+mod heap;
+
+use heap::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut heap = create_heap(10);
+
+    insert(&mut heap, 5);
+    insert(&mut heap, 3);
+    insert(&mut heap, 11);
+    insert(&mut heap, 24);
+    insert(&mut heap, 59);
+    insert(&mut heap, 1);
+
+    print_nodes(&heap);
+
+    delete_min(&mut heap);
+    delete_min(&mut heap);
+    delete_min(&mut heap);
+    delete_min(&mut heap);
+
+    print_nodes(&heap);
 }
